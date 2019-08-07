676375b22f6c3fdf1fdda779ae7ec5ec50dcabac
Jirapat Wisawanart <jirapat.wi@student.chula.ac.th>
2019-08-08 01:33:50 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+
 //Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-//Start the app by listening on the default Herokuport
+app.use(express.static(__dirname + '/dist/js-test'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/js-test/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
